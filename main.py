c94e78c23ab241d6d88ce818fc321cb7337cf701
apenaranda <alejandropenaranda3001@gmail.com>
2023-03-25 15:43:19 -0500

    se agrego el raton y el queso en el board

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import pygame , sys
 import random
 
-#-----------------
-mouse = {'x':0, 'y':0}
-
-
-#-----------------
-
 def movements_table (left_sen, up_sen, right_sen,down_sen, hq):
     # the movements will be represented by numbers  1 = up, 2 = left, 3 = down, 4 = right
     # when the mouse found the cheese, this will be represented by the number 5 = found cheese
@@ -82,14 +76,21 @@ def generate_matrix(n,m):
     for i in range(n):
         matriz.append([])
         for h in range(m):
-            matriz[i].append(wall_generator())
+            if i == mouse.get('y') and h == mouse.get('x'):
+                matriz[i].append(1) 
+            elif i == queso.get('y') and h == queso.get('x'):
+                matriz[i].append(1) 
+            else:
+                matriz[i].append(wall_generator())
     return matriz
 
 #funcion retorna 1 o 0 dependiendo del valor que se genere automaticamente
-# se le da mas posibillidad de devolver 0 para que no hayan muchas paredes
+#se le da mas posibillidad de devolver 1 para que no hayan muchas paredes
+#1 es un espacio libre para avanzar
+#0 no es un espacio libre
 def wall_generator():
-    numero = random.randint(0,8)
-    if numero == 0:
+    numero = random.randint(0,10)
+    if numero == 0 or numero == 8:
         return 0
     else:
         return 1
@@ -97,12 +98,11 @@ def wall_generator():
 #solo funciona para matrices nxn
 
 #se crea el tablero, nota si se actualiza el tablero se debe de referenciar otra vez ya que no esta en el while del refresco
-def create_board (matriz):
+def create_board (matriz,size):
     i = -1 #desplazamiento en las columnas
     j = 0  #desplazamiento en las filas
-    size = 90 #tamanho del cuadrado
     tamanho = len(matriz) #tamanho de la matriz
-    aux = 25 #corrimiento de los cuadrados
+    aux = 0 #corrimiento de los cuadrados
     for rows in matriz:
         i = i+1
         for cells in rows:
@@ -119,28 +119,38 @@ def create_board (matriz):
             if (i==tamanho):
                 break
     return True
-            
-#1 es un espacio libre para avanzar
-#0 no es un espacio libre
-#2 es la rata
-matriz =[[1,1,0,1,1],
-         [0,1,1,1,0],
-         [1,1,0,1,1],
-         [1,1,1,1,1],
-         [0,1,0,2,0]]
+#-----------------
 
 # tamaño de las filas y columnas
 n = 5
 m = 5
+
+#-----------------
+def generate_rata():
+    mouse = {'x':0, 'y':0}
+    mouse.update({'x':random.randint(0,n-1), 'y':random.randint(0,m-1)})
+    return  mouse
+mouse = generate_rata()
+
+def generate_queso():
+    queso = {'x':0, 'y':0}
+    queso.update({'x':random.randint(0,n-1), 'y':random.randint(0,m-1)})
+    if(queso == mouse):
+        generate_queso()
+    return queso
+    
+queso = generate_queso()
 
 #se inicia la aplicacion
 pygame.init()
 
 
 #se carga la imagen del raton y demas
-mouseImage = pygame.image.load('imagenes/prueba1.png')
-roadImage = pygame.image.load('imagenes/road.jpg')
+mouseImage = pygame.image.load('imagenes/rata2.png')
+cheeseImage = pygame.image.load('imagenes/queso.png')
+roadImage = pygame.image.load('imagenes/road1.png')
 wallImage = pygame.image.load('imagenes/wall.jpg')
+imgsize = 90
 
 #Definir colores
 black = (0,0,0)
@@ -150,7 +160,9 @@ green = (0,255,0)
 white = (255,255,255)
 
 #tamanho de la GUI
-size= (500,500)
+aux1 = n*imgsize
+aux2 = m*imgsize
+size = (aux1,aux2)
 
 #definicion de la GUI
 screen = pygame.display.set_mode(size)
@@ -160,8 +172,9 @@ screen.fill(white)
 
 #llamado de la funcion tablero
 #create_board(matriz)
-create_board(generate_matrix(n,m))
-screen.blit(mouseImage, ((mouse.get('x')*90)+25,(mouse.get('y')*90)+25))
+create_board(generate_matrix(n,m),imgsize)
+screen.blit(mouseImage, ((mouse.get('x')*imgsize),(mouse.get('y')*imgsize)))
+screen.blit(cheeseImage, ((queso.get('x')*imgsize),(queso.get('y')*imgsize)))
 
 #while para la logica o los eventos
 while True:
